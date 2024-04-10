15e031bb18c3293daab86931329b026bf5238369
jahanRazh <jahanrazh@gmail.com>
2024-04-10 23:48:06 +0530

    router path update vehicle

--- Backend/server.js
+++ Backend/server.js
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/users", router);
+app.use("/vehicles",router);
 app.use(cors());
 
 //routes
