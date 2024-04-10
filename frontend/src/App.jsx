0a3c05c2b071678acb703f0d9790aa8437bee3dc
pathumtharaka2000 <it22600516@my.sliit.lk>
2024-04-10 23:58:36 +0530

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,39 @@
-import { BrowserRouter,Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home';
-import UserManage from './Pages/UserManage'
-import AllUsers from './Pages/AllUsers';
-import AddUsers from './Pages/AddUsers';
-import NoPages from './Pages/NoPages';
-import Supplier from './Pages/Supplier';
 
-
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import UserManage from "./Pages/UserManage";
+import AllUsers from "./Pages/AllUsers";
+import AddUsers from "./Pages/AddUsers";
+import NoPages from "./Pages/NoPages";
+import Supplier from "./Pages/Supplier";
+import EmpManagement from "./Pages/EmpManagement";
+import PaySlip from "./Pages/PaySlip";
+import LeaveMng from "./Pages/leavemng";
+import Attendance from "./Pages/Attendance-mng";
+import LeaveForm from "./Pages/LeaveForm";
+import ActiveLeaves from "./Pages/ActiveLeaves";
+import UpdateLeave from "./Pages/UpdateLeave";
 
 function App() {
   
   return (
     <div>
       <BrowserRouter>
-      <Routes>
-
-        <Route index element = {<Home/>}/>
-        <Route path = '/dashboard' element = {<Home/>}/>
-        <Route path = '/user' element = {<UserManage/>}/>
-        <Route path = '/user/addusers' element = {<AddUsers/>}/>
-        <Route path = '/user/allusers' element = {<AllUsers/>}/>
-        <Route path = '/supplier' element = {<Supplier/>}/>
-        <Route path = '*' element = {<NoPages/>}/>
-
-
-
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/user" element={<UserManage />} />
+          <Route path="/user/addusers" element={<AddUsers />} />
+          <Route path="/user/allusers" element={<AllUsers />} />
+          <Route path="/supplier" element={<Supplier />} />
+          <Route path="*" element={<NoPages />} />
+          <Route path="/employee-management" element={<EmpManagement />} />
+          <Route path="/employee-management/PaySlip" element={<PaySlip />} />
+          <Route path="/employee-management/Leaves" element={<LeaveMng />} />
+          <Route path="/employee-management/Attendance"element={<Attendance />}/>
+          <Route path="/LeaveForm"element={<LeaveForm />}/>
+          <Route path="/ActiveLeaves"element={<ActiveLeaves />}/>
+          <Route path="/updateLeave" element={<UpdateLeave/>}/>
 
 
         </Routes>
