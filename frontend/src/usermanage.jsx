77529158c61dd834497185e59c27ad336277ddee
Ushara Wijesinghe <127342854+Ushara-B@users.noreply.github.com>
2024-04-11 19:39:14 +0530

    Update usermanage.jsx

--- frontend/src/usermanage.jsx
+++ frontend/src/usermanage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Menu from '../Components/menu';
+import AppBar from '../Components/Appbar';
 
 const usermanage = () => {
   return (
     <div>
+        <AppBar />
+        <Menu/>
       
     </div>
   )
