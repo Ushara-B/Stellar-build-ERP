78cda1bd5dfe03cd6a95234e54031799a3a6c7cf
jahanRazh <jahanrazh@gmail.com>
2024-04-11 11:06:11 +0530

    update server
    router path and cors line update

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const { readdirSync } = require("fs");
-const router = require("./routes/userRouters");
-
+const userrouter = require("./routes/userRouters");
+const vehiclerouter = require("./routes/VehicleRoutes");
 const app = express();
 
 const dotenv = require("dotenv").config();
@@ -15,9 +15,10 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use("/users", router);
-app.use("/vehicles",router);
 app.use(cors());
+app.use("/users", userrouter);
+app.use("/vehicles",vehiclerouter);
+
 
 //routes
 readdirSync("./routes").map((route) =>
