7fc62ec22828ad538865fee829456db76dcbfe0f
Mihiranga Fernando <it22585394@my.sliit.lk>
2024-04-15 09:19:57 +0530

    backend Leave

---
+++ Backend/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+const vehiclerouter = require("./routes/VehicleRoutes");
+const projectrouter = require("./routes/projectRoute");
+const userrouter = require("./routes/userRouters");
+
+const app = express();
+
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 5000;
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/users", userrouter);
+app.use("/vehicles",vehiclerouter);
+
+app.use("/projects", projectrouter);
+
+
+
+//routes
+readdirSync("./routes").map((route) =>
+  app.use("/api/v1", require("./routes/" + route))
+);
+
+//connect to DB and start server
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server Running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
