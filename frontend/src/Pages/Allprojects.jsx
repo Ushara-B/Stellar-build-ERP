3ffd01994323d9d0d196d0a997053c87fdaa8177
pathumtharaka2000 <it22600516@my.sliit.lk>
2024-04-20 23:44:44 +0530

    all project design
    design all project page

--- frontend/src/Pages/Allprojects.jsx
+++ frontend/src/Pages/Allprojects.jsx
@@ -8,151 +8,313 @@ import SearchIcon from '@mui/icons-material/Search';
 import PrintIcon from '@mui/icons-material/Print';
 import AddIcon from '@mui/icons-material/Add';
 import IconButton from '@mui/material/IconButton';
-import { Box, Paper, InputBase, TableContainer, Table, TableHead, TableBody, TableRow, TableCell, TablePagination } from '@mui/material';
+import { Box, Paper, InputBase, TableContainer, Table, TableHead, TableBody, TableRow, TableCell, TablePagination, Breadcrumbs, Link, Typography, Divider } from '@mui/material';
+import MilestoneBar from './MilestoneBar';
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 
 const URL = "http://localhost:5000/projects";
 
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+
 const fetchHandler = async () => {
-    return await axios.get(URL).then((res) => res.data);
+  return await axios.get(URL).then((res) => res.data);
 }
 
-function AllProjects() {
-    const [projects, setProjects] = useState([]);
-    const [searchQuery, setSearchQuery] = useState("");
-    const [noResults, setNoResults] = useState(false);
-    const navigate = useNavigate();
-    const ComponentsRef = useRef();
-    const ComponentsRefsingle = useRef();
+export default function AllProjects() {
+  const [projects, setProjects] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [noResults, setNoResults] = useState(false);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const navigate = useNavigate();
+  const ComponentsRef = useRef();
+  const ComponentsRefsingle = useRef();
 
-    useEffect(() => {
-        fetchHandler().then((data) => setProjects(data.project));
-    }, []);
+  useEffect(() => {
+    fetchHandler().then((data) => setProjects(data.project));
+  }, []);
 
-    const handlePrint = useReactToPrint({
-        content: () => ComponentsRef.current,
-        documentTitle: 'All Projects Report',
-        onAfterPrint: () => alert("All Projects Report successfully Downloaded!"),
+  const handlePrint = useReactToPrint({
+    content: () => ComponentsRef.current,
+    documentTitle: 'All Projects Report',
+    onAfterPrint: () => alert("All Projects Report successfully Downloaded!"),
+  });
+
+  const handlePrintSingle = useReactToPrint({
+    content: () => ComponentsRefsingle.current,
+    documentTitle: 'Project Report',
+    onAfterPrint: () => alert("Project Report successfully Downloaded!"),
+  });
+
+  const handleSearch = () => {
+    fetchHandler().then((data) => {
+      const filteredProjects = data.project.filter((project) =>
+        Object.values(project).some((field) =>
+          field.toString().toLowerCase().includes(searchQuery.toLowerCase())
+        ));
+      setProjects(filteredProjects);
+      setNoResults(filteredProjects.length === 0);
     });
+  };
 
-    const handlePrintSingle = useReactToPrint({
-        content: () => ComponentsRefsingle.current,
-        documentTitle: 'Project Report',
-        onAfterPrint: () => alert("Project Report successfully Downloaded!"),
-    });
+  const handleAddClick = () => {
+    navigate(`/Newprojects`);
+  };
 
-    const handleSearch = () => {
-        fetchHandler().then((data) => {
-            const filteredProjects = data.project.filter((project) =>
-                Object.values(project).some((field) =>
-                    field.toString().toLowerCase().includes(searchQuery.toLowerCase())
-                ));
-            setProjects(filteredProjects);
-            setNoResults(filteredProjects.length === 0);
-        });
-    };
+  const deleteHandler = async (_id) => {
+    try {
+      await axios.delete(`http://localhost:5000/projects/${_id}`);
+      setProjects(prevProjects => prevProjects.filter(project => project._id !== _id));
+    } catch (error) {
+      console.error("Error deleting project:", error);
+    }
+  };
 
-    const handleAddClick = () => {
-        navigate(`/Newprojects`);
-    };
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-US');
+  };
 
-    const deleteHandler = async (_id) => {
-        try {
-            await axios.delete(`http://localhost:5000/projects/${_id}`);
-            setProjects(prevProjects => prevProjects.filter(project => project._id !== _id));
-        } catch (error) {
-            console.error("Error deleting project:", error);
-        }
-    };
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
 
-    const formatDate = (dateString) => {
-        const date = new Date(dateString);
-        return date.toLocaleDateString('en-US');
-    };
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
-    return (
-        <div style={{ marginLeft: '250px', paddingTop: '80px' }}>
-            <AppBar />
-            <Menu />
-            <Box sx={{ display: 'flex', alignItems: 'center', marginRight: '40px' }}>
-                <InputBase
-                    sx={{ flex: 1, marginLeft: '10px' }}
-                    placeholder="Search project Details"
-                    value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)}
-                    startAdornment={<SearchIcon />}
-                />
-                <IconButton color="primary" aria-label="search" onClick={handleSearch}>
-                    <SearchIcon />
-                </IconButton>
-                <IconButton color="primary" aria-label="add project" onClick={handleAddClick}>
-                    <AddIcon />
-                </IconButton>
-                <IconButton color="primary" aria-label="print all" onClick={handlePrint}>
-                <PrintIcon />
-                </IconButton>
-            </Box>
+  const calculateMilestoneProgress = (project) => {
+    const startDate = new Date(project.startDate);
+    const endDate = new Date(project.endDate);
+    const currentDate = new Date();
 
-            {noResults ? (
-                <div>
-                    <p>No results found</p>
-                </div>
-            ) : (
-                <div>
-                    <TableContainer component={Paper}>
-                        <Table>
-                            <div ref={ComponentsRef}>
-                            <h1>Details of Projects</h1>
-                            <TableHead>
-                                <TableRow
-                                 sx={{
-                                   backgroundColor: '#b1c5d4',
-                                   fontWeight: 'bold',
-                                   border: 'none',
-                                   padding: '5px 10px',
-                                   '&:hover': {
-                                     backgroundColor: '#b1c5d4',
-                                     
-                                   },
-                                 }}>
-                                    <TableCell>Project Name</TableCell>
-                                    <TableCell>Project Budget</TableCell>
-                                    <TableCell>Employees</TableCell>
-                                    <TableCell>Status</TableCell>
-                                    <TableCell>Start Date</TableCell>
-                                    <TableCell>End Date</TableCell>
-                                    <TableCell>Project Type</TableCell>
-                                    <TableCell>Action</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {projects.map((project) => (
-                                    <TableRow ref={ComponentsRefsingle} key={project._id}>
-                                        <TableCell>{project.projectName}</TableCell>
-                                        <TableCell>{project.projectBudget}</TableCell>
-                                        <TableCell>{project.Employees}</TableCell>
-                                        <TableCell>{project.Status}</TableCell>
-                                        <TableCell>{formatDate(project.startDate)}</TableCell>
-                                        <TableCell>{formatDate(project.endDate)}</TableCell>
-                                        <TableCell>{project.projectType}</TableCell>
-                                        <TableCell>
-                                            <IconButton onClick={() => navigate(`/Updateprojects/${project._id}`)} className="update-button">Update</IconButton>
-                                            <IconButton onClick={() => deleteHandler(project._id)} className="delete-button">Delete</IconButton>
-                                            <IconButton onClick={() => handlePrintSingle()} className="report-button">Report</IconButton>
-                                        </TableCell>
-                                    </TableRow>
-                                ))}
-                            </TableBody>
-                            </div>
-                        </Table>
-                    </TableContainer>
-                </div>
-            )}
-            <br /><br />
-        </div>
-    )
+    if (currentDate < startDate) {
+      return 0; // Project hasn't started yet
+    } else if (currentDate >= endDate) {
+      return 100; // Project is completed
+    } else {
+      const totalDays = (endDate - startDate) / (1000 * 60 * 60 * 24);
+      const elapsedDays = (currentDate - startDate) / (1000 * 60 * 60 * 24);
+      const progress = (elapsedDays / totalDays) * 100;
+      return Math.round(progress);
+    }
+  };
+
+  return (
+    <>
+    <AppBar />
+    <Menu />
+    <div style={{ marginLeft: '250px', paddingTop: '20px' }}>
+     
+      <Box sx={{ p: 9 }} height={2}>
+        <Breadcrumbs
+          arial-label="breadcrumb"
+          separator={<NavigateNextIcon fontSize="small" />}
+        >
+          <Link underline="hover" key="1" color="inherit" href="/projects">
+            Projects
+          </Link>
+          <Typography key="3" color="text.primary">
+            All Projects
+          </Typography>
+        </Breadcrumbs>
+
+        <Paper sx={{ width: "98%", overflow: "hidden", padding: "12px" }}>
+          <Typography
+            gutterBottom
+            variant="h4"
+            component="div"
+            sx={{ padding: "20PX" }}
+          >
+            All Projects
+          </Typography>
+          <Divider />
+
+          <br />
+          <Box sx={{ display: 'flex', alignItems: 'center', marginRight: '40px' }}>
+            <InputBase
+              sx={{ flex: 1, marginLeft: '10px' }}
+              placeholder="Search project Details"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              startAdornment={<SearchIcon fontSize="small" />}
+            />
+            <Box>
+            <IconButton color="primary" aria-label="search" onClick={handleSearch}>
+              <SearchIcon />
+            </IconButton>
+            <IconButton color="primary" aria-label="add project" onClick={handleAddClick}>
+              <AddIcon />
+            </IconButton>
+            <IconButton color="primary" aria-label="print all" onClick={handlePrint}>
+              <PrintIcon />
+            </IconButton>
+          </Box>
+          </Box>
+
+
+          <TableContainer ref={ComponentsRef}>
+            <Table stickyHeader
+                aria-label="sticky table"
+                sx={{ borderCollapse: "collapse" }}
+              >
+             
+               
+                <TableHead>
+                  <TableRow
+                    sx={{
+                      backgroundColor: '#b1c5d4',
+                      fontWeight: 'bold',
+                      border: 'none',
+                      padding: '5px 10px',
+                      '&:hover': {
+                        backgroundColor: '#b1c5d4',
+                      },
+                    }}>
+                    <TableCell>Project Name</TableCell>
+                    <TableCell>Project Budget</TableCell>
+                    <TableCell>Employees</TableCell>
+                    <TableCell>Status</TableCell>
+                    <TableCell>Start Date</TableCell>
+                    <TableCell>End Date</TableCell>
+                    <TableCell>Project Type</TableCell>
+                    <TableCell>Milestone</TableCell>
+                    <TableCell>Action</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {projects
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((row) => (
+                      <TableRow ref={ComponentsRefsingle} key={row._id}
+                        sx={{
+                          "&:hover": {
+                            backgroundColor: "#f5f5f5",
+                          },
+                          border: "none",
+                          padding: "8px 16px",
+                        }}
+                      >
+                        <TableCell
+                          sx={{
+                            border: "1px",
+                            padding: "10px 12px",
+                            backgroundColor: "white",
+                          }}
+                        >
+                          {row.projectName}
+                        </TableCell>
+                        <TableCell
+                          sx={{
+                            border: "1px",
+                            padding: "10px 12px",
+                            backgroundColor: "white",
+                          }}
+                        >
+                          {row.projectBudget}
+                        </TableCell>
+                        <TableCell
+                          sx={{
+                            border: "1px",
+                            padding: "10px 12px",
+                            backgroundColor: "white",
+                          }}
+                        >
+                          {row.Employees}
+                        </TableCell>
+                        <TableCell
+                          sx={{
+                            border: "1px",
+                            padding: "10px 12px",
+                            backgroundColor: "white",
+                          }}
+                        >
+                          {row.Status}
+                        </TableCell>
+                        <TableCell
+                          sx={{
+                            border: "1px",
+                            padding: "10px 12px",
+                            backgroundColor: "white",
+                          }}
+                        >
+                          {formatDate(row.startDate)}
+                        </TableCell>
+                        <TableCell
+                          sx={{
+                            border: "1px",
+                            padding: "10px 12px",
+                            backgroundColor: "white",
+                          }}
+                        >
+                          {formatDate(row.endDate)}
+                        </TableCell>
+                        <TableCell
+                          sx={{
+                            border: "1px",
+                            padding: "10px 12px",
+                            backgroundColor: "white",
+                          }}
+                        >
+                          {row.projectType}
+                        </TableCell>
+                        <TableCell
+                          sx={{
+                            border: "1px",
+                            padding: "10px 12px",
+                            backgroundColor: "white",
+                          }}
+                        >
+                          <MilestoneBar progress={calculateMilestoneProgress(row)} />
+                        </TableCell>
+                        <TableCell
+                          sx={{
+                            border: "1px",
+                            padding: "10px 12px",
+                            backgroundColor: "white",
+                          }}
+                        >
+                          <IconButton onClick={() => navigate(`/Updateprojects/${row._id}`)} className="update-button">Update</IconButton>
+                          <IconButton onClick={() => deleteHandler(row._id)} className="delete-button">Delete</IconButton>
+                          <IconButton onClick={() => handlePrintSingle()} className="report-button">Report</IconButton>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                </TableBody>
+
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[10, 25, 100]}
+            component="div"
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+            sx={{
+              borderTop: "none",
+              padding: "12px 16px",
+            }}
+          />
+        </Paper>
+      </Box>
+    </div>
+    </>
+  );
 }
 
-export default AllProjects;
 
 
 /*
