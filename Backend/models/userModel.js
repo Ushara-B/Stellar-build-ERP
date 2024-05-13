b0836cc3c06ba9c77873c895638a6339896b7df3
Ushara Wijesinghe <127342854+Ushara-B@users.noreply.github.com>
2024-05-14 02:22:33 +0530

    update user authentication

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -1,75 +1,25 @@
-const mongoose = require ("mongoose");
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
 
 const userSchema = new Schema({
-    user_N:{
-        type : String,
-        required:true,
-    },
-    f_Name:{
-        type : String,
-        required:true,
-    },
-
-    l_Name:{
-        type : String,
-        required:true,
-    },
-
-    age:{
-        type : Number,
-        required:true,
-    },
-    email:{
-        type : String,
-        required:true,
-    },
-
-    address:{
-        type : String,
-        required:true,
-    },
-    
-    dob:{
-        type : Date,
-        required:true,
-    },
-    gender:{
-        type : String,
-        required:true,
-    },
-    m_Status:{
-        type : String,
-        required:true,
-    },
-    nic:{
-        type : String,
-        required:true,
-    },
-    role:{
-        type : String,
-        required:true,
-    },
-    pswrd:{
-        type : String,
-        required:true,
-    },
-    contact_No:{
-        type : String,
-        required:true,
-    },
-    f_contactNo:{
-        type : String,
-        required:true,
-    },
-    bank_D:{
-        type : String,
-        required:true,
-    },
-    
+  user_N: { type: String, required: true, unique: true },
+  f_Name: { type: String, required: true },
+  l_Name: { type: String, required: true },
+  age: { type: Number, required: true },
+  email: { type: String, required: true, unique: true },
+  address: { type: String, required: true },
+  dob: { type: Date, required: true },
+  gender: { type: String, required: true },
+  m_Status: { type: String, required: true },
+  nic: { type: String, required: true },
+  role: { type: String, required: true },
+  pswrd: { type: String, required: true },
+  contact_No: { type: String, required: true },
+  f_contactNo: { type: String, required: true },
+  bank_D: { type: String, required: true },
 });
 
-module.exports = mongoose.model(
-   "userModel", //file name
-   userSchema //function name
-)
+
+
+module.exports = mongoose.model("userModel", userSchema);
