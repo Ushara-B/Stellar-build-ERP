7e30df16e92683d7667e3986f297acc45f50e918
Ushara Wijesinghe <127342854+Ushara-B@users.noreply.github.com>
2024-05-13 15:17:54 +0530

    update user controller and userRouter file

--- Backend/routes/userRouters.js
+++ Backend/routes/userRouters.js
@@ -3,31 +3,6 @@ const router = express.Router();
 const User = require("../models/userModel");
 const UserController = require("../controllers/userControllers");
 
-// Login route
-router.post("/login", async (req, res) => {
-  const { usernameOrEmail, password } = req.body;
-
-  try {
-    // Find the user by user_N or email
-    const user = await User.findOne({
-      $or: [{ user_N: usernameOrEmail }, { email: usernameOrEmail }],
-    });
-
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
-    }
-
-    // Check if the password matches
-    if (user.pswrd === password) {
-      return res.status(200).json({ user });
-    } else {
-      return res.status(401).json({ error: "Incorrect password" });
-    }
-  } catch (error) {
-    console.error("Error logging in:", error);
-    return res.status(500).json({ error: "Internal server error" });
-  }
-});
 
 // Other user routes
 router.get("/", UserController.getAllUsers);
@@ -35,5 +10,6 @@ router.post("/", UserController.addUsers);
 router.get("/:id", UserController.getById);
 router.put("/:id", UserController.updateUser);
 router.delete("/:id", UserController.deleteUser);
+router.post("/login", UserController.login);
 
 module.exports = router;
